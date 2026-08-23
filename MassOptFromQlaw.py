d58ff7e0b0a276a4df8840fc0e743c75786ea1a1
BANANAMOB <60293446+BANANAMOB@users.noreply.github.com>
2026-08-23 19:25:40 -0400

    Add files via upload

--- MassOptFromQlaw.py
+++ MassOptFromQlaw.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.linalg as lin
 import asset_asrl as ast
 import matplotlib.pyplot as plt
 import scipy
@@ -10,7 +11,7 @@ from colorama import Fore
 from colorama import Style
 colorama_init()
 import random
-from math import inf
+from math import inf, sin, cos, sqrt
 from rich import print as rprint
 import time
 from scipy.integrate import solve_ivp
@@ -26,8 +27,8 @@ optType = "LGL3"
 E_or_T = 1
 numKnots = 200
 numThreads = 16
-MeshTol = 1.0e-8 # 1.0e-10
-EControl = 1.0e-10 # 1.0e-11
+MeshTol = 1.0e-6 # 1.0e-10
+EControl = 1.0e-8 # 1.0e-11
 
 # setting canonical distance unit
 #DU = 384400000.0000000
@@ -36,7 +37,7 @@ EControl = 1.0e-10 # 1.0e-11
 #TU = 2.360584684800000E+06/(2*np.pi)
 
 # earth 
-mu = 398600.49e9
+mu = 398600.49 # km # 398600.49e9 in m
 
 # minimum control profile set to aid ASSET convergence 
 Umin = 1.0e-8
@@ -45,36 +46,39 @@ Umin = 1.0e-8
 # MEE Gauss variational equations
 class MEE_gve(oc.ODEBase):
 
-    def __init__(self,mu):
-        Xvars = 6
+    def __init__(self,mu,Isp):
+        Xvars = 7
         Uvars = 3
         ####################################################
         XtU = oc.ODEArguments(Xvars,Uvars)
 
-        a, f, g, h, k, L = XtU.XVec().tolist()
+        a, f, g, h, k, L, m = XtU.XVec().tolist()
         f_r, f_t, f_h = XtU.UVec().tolist()
 
-        p = a * (1 - f^2 - g^2)
-        q = 1 + f*np.cos(L) + g*np.sin(L)
+        p = a * (1 - f**2 - g**2)
+        q = 1 + f*vf.cos(L) + g*vf.sin(L)
 
-        a_dot = 2 * np.sqrt(a^3 / (mu*(1-f^2-g^2))) * \
-                ( (f*np.sin(L) - g*np.cos(L))*f_r + q*f_t )
-        f_dot = 1/q * np.sqrt(p/mu) * \
-                ( (q*np.sin(L))*f_r + ((q+1)*np.cos(L)+f)*f_t + -g*(h*np.sin(L)-k*np.cos(L))*f_h )
-        g_dot = 1/q * np.sqrt(p/mu) * \
-                ( (-q*np.cos(L))*f_r + ((q+1)*np.sin(L)+g)*f_t + f*(h*np.sin(L)-k*np.cos(L))*f_h )
-        h_dot = 1/q * np.sqrt(p/mu) * \
-                ( 0.5*(1+h^2+k^2)*np.cos(L)*f_h )
-        k_dot = 1/q * np.sqrt(p/mu) * \
-                ( 0.5*(1+h^2+k^2)*np.sin(L)*f_h )
-        L_dot = 1/q * np.sqrt(p/mu) * \
-                ( (h*np.sin(L)-k*np.cos(L))*f_h ) + np.sqrt(mu*p) * (q/p)^2
+        a_dot = 2 * vf.sqrt(a**3 / (mu*(1-f**2-g**2))) * \
+                ( (f*vf.sin(L) - g*vf.cos(L))*f_r + q*f_t )
+        f_dot = 1/q * vf.sqrt(p/mu) * \
+                ( (q*vf.sin(L))*f_r + ((q+1)*vf.cos(L)+f)*f_t + -g*(h*vf.sin(L)-k*vf.cos(L))*f_h )
+        g_dot = 1/q * vf.sqrt(p/mu) * \
+                ( (-q*vf.cos(L))*f_r + ((q+1)*vf.sin(L)+g)*f_t + f*(h*vf.sin(L)-k*vf.cos(L))*f_h )
+        h_dot = 1/q * vf.sqrt(p/mu) * \
+                ( 0.5*(1+h**2+k**2)*vf.cos(L)*f_h )
+        k_dot = 1/q * vf.sqrt(p/mu) * \
+                ( 0.5*(1+h**2+k**2)*vf.sin(L)*f_h ) 
+        L_dot = 1/q * vf.sqrt(p/mu) * \
+                ( (h*vf.sin(L)-k*vf.cos(L))*f_h ) + vf.sqrt(mu*p) * (q/p)**2
+        
+        g = 9.81
+        m_dot = -(m*vf.sqrt(f_r**2 + f_t**2 + f_h**2)) / (g*Isp)
     
-        ode = vf.stack([a_dot, f_dot, g_dot, h_dot, k_dot, L_dot])
+        ode = vf.stack([a_dot, f_dot, g_dot, h_dot, k_dot, L_dot, m_dot])
         ####################################################
         super().__init__(ode,Xvars,Uvars)
 
-def find_traj(mee_0, mee_T, Umax, IG, ode):
+def find_traj(mee_0, mee_T, time_0, time_max, Umax, IG, ode):
 
     a_0, f_0, g_0, h_0, k_0, L_0 = mee_0
     a_T, f_T, g_T, h_T, k_T, _ = mee_T
@@ -82,12 +86,12 @@ def find_traj(mee_0, mee_T, Umax, IG, ode):
     start_time = time.process_time()
 
     try: 
-        BoundaryFirst = list([a_0, f_0, g_0, h_0, k_0, L_0]) 
-        BoundaryLast =  list([a_T, f_T, g_T, h_T, k_T, 0]) 
+        BoundaryFirst = list([a_0, f_0, g_0, h_0, k_0, L_0]) + [time_0]
+        BoundaryLast =  list([a_T, f_T, g_T, h_T, k_T, 0]) + [time_max]
 
         phase1 = run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, 0, Umax, Umin, numKnots, numThreads, MeshTol, EControl)
         Traj1 = phase1.returnTraj()
-        dV_e, f_e, TrajI_e = reintegration(ode,phase1,Traj1,Umax)
+        dV_e, f_e, TrajI_e = reintegration(ode,phase1,Traj1,Umax,mee_T)
 
         time_cpu_e = time.process_time()-start_time
 
@@ -105,7 +109,7 @@ def find_traj(mee_0, mee_T, Umax, IG, ode):
         IG = Traj1
         phase2 = run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, Umin, numKnots, numThreads, MeshTol, EControl)
         Traj2 = phase2.returnTraj()
-        dV_m, f_m, TrajI_m = reintegration(ode,phase2,Traj2,Umax)
+        dV_m, f_m, TrajI_m = reintegration(ode,phase2,Traj2,Umax,mee_T)
 
         time_cpu_m = time.process_time()-start_time
 
@@ -120,17 +124,19 @@ def find_traj(mee_0, mee_T, Umax, IG, ode):
             converged_m = 0
             Traj_data_m = [converged_m,TrajI_m,mee_0,mee_T,0,time_cpu_m,f_m]
     
-    except:
+    except Exception as e:
         # save result
+        print(Fore.RED + "Error")
+        print(e)
         converged_e = 0
         converged_m = 0
         time_cpu = start_time-time.process_time()
-        Traj_data_e = [converged_e,TrajI_e,mee_0,mee_T,0,time_cpu_e,-2]
-        Traj_data_m = [converged_m,TrajI_m,mee_0,mee_T,0,time_cpu_m,-2]
+        Traj_data_e = [converged_e,0,mee_0,mee_T,0,time_cpu,-2]
+        Traj_data_m = [converged_m,0,mee_0,mee_T,0,time_cpu,-2]
 
     return Traj_data_e, converged_e, Traj_data_m, converged_m
 
-def reintegration(ode,phase2,Traj,Umax):
+def reintegration(ode,phase2,Traj,Umax,mee_T):
     # REINTEGRATION STEP (VERY IMPORTANT!!!)
     try:
         Tab = phase2.returnTrajTable()
@@ -153,18 +159,21 @@ def reintegration(ode,phase2,Traj,Umax):
         # Here, I use Simpson's rule to approximate the integral over time (agrees to exact to <0.1% on all cases thus far)
         dV = scipy.integrate.simpson(Thrust_Mag, x=np.reshape(TrajI[:,6],(len(Thrust_Mag),1)), axis=0)[-1] # DU/TU *
 
-        if np.linalg.norm(TrajI[-1][:6] - TrajI[0][:6]) < MeshTol*10 and max(Thrust_Mag) < Umax:
+        if np.linalg.norm(TrajI[-1][:5] - mee_T) < MeshTol*10 and max(Thrust_Mag) < Umax:
             f = 1
         else:
             f = 0
 
-    except:
+    except Exception as e:
+        # save result
+        print(Fore.RED + "Error")
+        print(e)
         dV = 0.0
         f = -1
 
     return dV, f, TrajI
 
-def run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, Umin, numKnots, numThreads, MeshTol, EControl,max_target_z):
+def run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, Umin, numKnots, numThreads, MeshTol, EControl):
     # try:
     phase = ode.phase(optType, IG, numKnots)
     # except:
@@ -177,8 +186,14 @@ def run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, U
     phase.addBoundaryValue("Last", range(0,5), BoundaryLast[0:5])
 
     # adding the time boundary conditions
-    phase.addBoundaryValue("First", [6], 0)
+    phase.addBoundaryValue("First", [6], BoundaryFirst[6])
     #phase.addBoundaryValue("Last", [6], BoundaryLast[3])
+    #phase.addLUVarBound("Last",[6], BoundaryFirst[6].item()+1, BoundaryLast[6].item())
+    #phase.addBoundaryValue("Last", [6], BoundaryLast[6].item()*10)
+
+    # Singularity account
+    #phase.addUpperVarBound("Path", [3], 1e10)
+    #phase.addUpperVarBound("Path", [4], 1e10)
 
     # constrain the initial and terminal position and velocities to be the same after one period of the forced-periodic trajectory 
     def FrontBackEqCon():
@@ -212,10 +227,11 @@ def run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, U
     # phase.addInequalCon("Path",coneIneqCon(),[0,1,2]) 
 
     # Produce a mass-optimal result by integrating over a norm() applied to the thrust vector
+    U = 1e5
     if E_or_T == 0:
-        phase.addIntegralObjective(Args(3).squared_norm(),[7,8,9]) 
+        phase.addIntegralObjective(Args(3).squared_norm(), [7,8,9]) 
     else:
-        phase.addIntegralObjective(Args(3).norm(),[7,8,9])
+        phase.addIntegralObjective(Args(3).norm(), [7,8,9])
 
     #XtUVars = [2]
     #OPVars = []
@@ -236,10 +252,10 @@ def run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, U
     ## Make sure to set optimizer EContol to be the same as or smaller than MeshTol
     phase.optimizer.set_EContol(EControl)
     ## Set Max number of mesh iterations:
-    phase.setMaxMeshIters(10)  # 5 is too low, try  7 default = 10
+    phase.setMaxMeshIters(15)  # 5 is too low, try  7 default = 10
      
     phase.setThreads(numThreads,numThreads) #Q: what does this do? A:Parallelization
-    phase.optimizer.set_PrintLevel(5) # Verbosity
+    phase.optimizer.set_PrintLevel(1) # Verbosity
     phase.optimize()
 
     return phase 
@@ -253,7 +269,14 @@ def save_pkl_mat(filename,data):
         pickle.dump(data,f)
     savemat(f'/home/lha43/Desktop/Manifold/DATA/{filename}.mat', {f'{filename}': data})
 
-def plot_orbit(Traj,ax0,ax1,ax2,ax3,ax4,col): # ,IG):
+def plot_orbit(Traj,col): # ,IG):
+
+    fig = plt.figure()
+    ax0 = plt.subplot(421)
+    ax1 = plt.subplot(423)
+    ax2 = plt.subplot(425)
+    ax3 = plt.subplot(427)
+    ax4 = plt.subplot(122, projection='3d')
 
     DU = 384400000.0000000
     TU = 2.360584684800000E+06/(2*np.pi)
@@ -311,45 +334,103 @@ def plot_orbit(Traj,ax0,ax1,ax2,ax3,ax4,col): # ,IG):
 
 if __name__ == "__main__":
 
+    Isp = 3100 # 2000
+    ode = MEE_gve(mu,Isp)
+
+    case = "A"
+
     ### Extract Q-law initial guess
     fileparent = f"/home/lha43/Desktop/Qlaw/IG_DATA"
 
-    filepath = f"{fileparent}/q_law_case_E_mee_traj_rv_toPy.jld2"
+    filepath = f"{fileparent}/q_law_case_{case}_mee_traj_mee_toPy_eta0.jld2"
+    with h5py.File(filepath, 'r') as f: 
 
+        n = len(f["m_traj"])
+        
+        mee_IG = np.array(f["mee_traj"])
+        u_IG = np.array(f["u_traj"])
+        m_IG = np.array(f["m_traj"]).reshape(n,1)
+        t_IG = np.array(f["time"]).reshape(n,1)
+
+    mee_IG[:,0] = mee_IG[:,0]/1000 # to KM)
+
+    filepath = f"{fileparent}/q_law_case_{case}_mee_traj_rv_toPy_eta0.jld2"
     with h5py.File(filepath, 'r') as f: 
         
-        r0 = np.array(f["r_traj"])
-        u0 = np.array(f["u_traj"])
-        m0 = np.array(f["m_traj"])
-        t0 = np.array(f["time"])
+        r_IG_full = np.array(f["r_traj"])
+        v_IG_full = np.array(f["v_traj"])
 
-    # print(mee0.shape)
-    # print(u0.shape)
-    # print(m0.shape)
-    # print(t0.shape)
+    # Assemble IG (remove the last row - it's a duplicate)
+    Traj_IG_full = np.block([mee_IG,t_IG,u_IG,m_IG])[:-1,:]
 
-    # mee_0 = mee0[0]
-    # mee_T = mee0[-1]
-
-    # print(mee_0)
-    # print(mee_T)
-    
-    #input()
-
-    # Assemble IG
-    #Traj_IG = np.block([r0,v0,t0,u0])
+    ###########################################################
 
     # Set up Figure
     fig = plt.figure(figsize=(8,6))
     ax = fig.add_subplot(111, projection='3d')
 
     # plot Q-law reference
-    ax.scatter(0, 0, 0, color='blue', label='Earth')
-    ax.plot(r0.T[0], r0.T[1], r0.T[2], color='black',linewidth=0.5, label='Case E')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    ax.axis("equal")
-    ax.legend()
+    #ax.scatter(0, 0, 0, color='blue', label='Earth')
+
+    perc_of_traj = 0.0
+    idx = int(n*perc_of_traj)
+    Traj_test = np.block([r_IG_full,v_IG_full,t_IG,u_IG,m_IG])[:-1,:]
+    Traj_test = Traj_test[idx:,:]
+    plot_orbit(Traj_test,'#956081')
     plt.show()
+    #ax.plot(r_IG.T[0], r_IG.T[1], r_IG.T[2], color='black',linewidth=0.5, label='Case E')
+    #ax.set_xlabel('X')
+    #ax.set_ylabel('Y')
+    #ax.set_zlabel('Z')
+    #ax.axis("equal")
+    #ax.legend()
+    #plt.show()
+
+    ###########################################################
+
+    # Piece together an optimized phase
+    perc_of_traj = 0.998 # 0.998
+    idx = int(n*perc_of_traj)
+
+    mee_0 = mee_IG[idx]
+    mee_T = mee_IG[-1] 
     
+    time_0 = t_IG[idx]
+    time_max = t_IG[-1]
+
+    Umax = 3/1000 # in km # 2 N
+
+    # set all 0 control to Umin
+    Traj_IG = Traj_IG_full[idx:,:]
+    Unorm = lin.norm(Traj_IG[:,7:10],axis=1)
+    Umask = Unorm < Umin
+    Traj_IG[Umask, 7:10] = Umin * Traj_IG[Umask, 3:6]
+
+    # f_vals = Traj_IG[:, 1] 
+    # g_vals = Traj_IG[:, 2] 
+    # print("max f²+g²:", np.max(f_vals**2 + g_vals**2)) 
+    # print(np.any(np.isnan(Traj_IG)))
+    # print(np.any(np.isinf(Traj_IG)))
+    # input()
+
+    r_IG=r_IG_full[idx:,:]
+
+    # ax.plot(r_IG.T[0], r_IG.T[1], r_IG.T[2], color='black',linewidth=0.5, label='Case E')
+    # plt.show()
+
+    Traj_data_e, converged_e, Traj_data_m, converged_m = find_traj(mee_0, mee_T, time_0, time_max, Umax, Traj_IG, ode)
+
+    print(Fore.BLACK + 'Results:')
+    print(Traj_data_e)
+    print(converged_e)
+    print(Traj_data_m)
+    print(converged_m)
+
+    Traj_sol = Traj_data_m[1]
+    plot_orbit(Traj_sol,'#1e81eb')
+    plt.show()
+
+
+    #print(Traj_data_e)
+    #print(Traj_data_m)
+
